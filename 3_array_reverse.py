a891343623c4db6c21455aacc07015975694efa4
shrav20 <91880988+shrav20@users.noreply.github.com>
2022-07-10 14:08:18 +0530

    Update and rename array_reverse.py to 3_array_reverse.py

---
+++ 3_array_reverse.py
@@ -0,0 +1,2 @@
+p=list(map(int,input("Enter array elements: ").split()))
+print(*p[::-1])
--- array_reverse.py
+++
@@ -1,5 +0,0 @@
-l=(input("Enter a list: "))
-length=len(l)
-print("Array elements in reverse are: ")
-for i in range(-1,-length-1,-1):
-    print(l[i],end=' ')
