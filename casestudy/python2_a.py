788fddba069585b49dc5effd0309c937d0e201d5
shrav20 <91880988+shrav20@users.noreply.github.com>
2022-07-23 21:12:29 +0530

    Add files via upload

---
+++ casestudy/python2_a.py
@@ -0,0 +1,43 @@
+#OOPS using python 
+#2_a
+class Complex ():
+    def initComplex(self):
+        self.realPart = int(input("Enter the Real Part: "))
+        self.imgPart = int(input("Enter the Imaginary Part: "))            
+    def display(self):
+        print(self.realPart,"+",self.imgPart,"i", sep="")
+    def sum(self, c1, c2):
+        self.realPart = c1.realPart + c2.realPart
+        self.imgPart = c1.imgPart + c2.imgPart
+    def sub(self,c1,c2):
+        self.realPart = c1.realPart - c2.realPart
+        self.imgPart = c1.imgPart - c2.imgPart
+    def mul(self,c1,c2):
+        self.realPart = c1.realPart * c2.realPart
+        self.imgPart = c1.imgPart * c2.imgPart
+    def __ge__(self,c1,c2):
+        self_ge = (c1.realPart ** 2)+(c1.imgPart **2)
+        other_ge =(c2.realPart ** 2)+(c2.imgPart **2)
+        return self_ge>=other_ge
+c1 = Complex()
+c2 = Complex()
+c3 = Complex()
+print("Enter first complex number")
+c1.initComplex()
+print("First Complex Number: ", end="")
+c1.display()
+print("Enter second complex number")
+c2.initComplex()
+print("Second Complex Number: ", end="")
+c2.display()
+print("Sum of two complex numbers is ", end="")
+c3.sum(c1,c2)
+c3.display()
+print("Subtraction of two complex numbers is ", end="")
+c3.sub(c1,c2)
+c3.display()
+print("Multiplication of two complex numbers is ", end="")
+c3.mul(c1,c2)
+c3.display()
+print("c1>=c2",c1.realPart>=c2.realPart and c1.imgPart>=c2.imgPart)
+print("c1==c2",c1.realPart==c2.realPart and c1.imgPart==c2.imgPart)
